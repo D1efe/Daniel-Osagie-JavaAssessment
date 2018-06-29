6272ae3f1dae1d9ddf2f46ab46f4ccfdfe4f4d9f
D1efe <d.osagie95@hotmail.co.uk>
2018-06-29 09:11:28 +0100

    final test

---
+++ JavaAssessment/src/javaassessment/JavaAssessment.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaassessment;
+
+/**
+ *
+ * @author Admin
+ */
+public class JavaAssessment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
